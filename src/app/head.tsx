904771fa9641569faebce8592da58cb6deac7ed3
Bryan Matheus <bryan.matheusbmf@gmail.com>
2023-02-03 22:25:21 -0300

    fix: correct SEO

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -5,8 +5,23 @@ export default function Head() {
     <>
       <title>Bryan Matheus</title>
       <meta content='width=device-width, initial-scale=1' name='viewport'/>
-      <meta name='description' content='Bryan Matheus | Front-end Engineer'/>
-      <link rel='icon' href='/favicon.webp'/>
+      <meta
+        name='description'
+        content='Skilled Front-end Engineer. Proficient in JavaScript, TypeScript, React, & Node. Passionate about creating user-friendly web apps.'/>
+      <meta property='og:title' content='Bryan Matheus | Front-end Engineer'/>
+      <meta property='og:type' content='website'/>
+      <meta property='og:url' content='https://bmatz.vercel.app'/>
+      <meta
+        property='og:description'
+        content='Skilled Front-end Engineer. Proficient in JavaScript, TypeScript, React, & Node. Passionate about creating user-friendly web apps.'/>
+      <meta property='og:image' content='https://bmatz.vercel.app/me.png'/>
+      <link rel='apple-touch-icon' sizes='180x180' href='/apple-touch-icon.png'/>
+      <link rel='icon' type='image/png' sizes='32x32' href='/favicon-32x32.png'/>
+      <link rel='icon' type='image/png' sizes='16x16' href='/favicon-16x16.png'/>
+      <link rel='manifest' href='/site.webmanifest'/>
+      <link rel='mask-icon' href='/safari-pinned-tab.svg' color='#292929'/>
+      <meta name='msapplication-TileColor' content='#292929'/>
+      <meta name='theme-color' content='#292929'/>
     </>
   );
 }
